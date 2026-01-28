948285e594f8026ea6ac3d8da9f41cc540b497e8
Mariama Sakho <mariama.sakho@gmail.com>
2026-01-28 18:24:21 +0100

    Style delete button on My Trips

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :trips, only: %i[index new create show edit update] do
+  resources :trips, only: %i[index new create show edit update destroy] do
     member do
       get  :loading
       get  :status
